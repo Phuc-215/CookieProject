12e55a7c0d934094ee0d2953a65e0632940f6e7b
vkb0205 <vkb0205@gmail.com>
2026-01-12 02:49:40 +0700

    change axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { getAccessToken } from '@/utils/token';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
+const BASE_URL = import.meta.env.VITE_API_URL; 
 
 const api = axios.create({
   baseURL: BASE_URL, // <--- Sửa ở đây

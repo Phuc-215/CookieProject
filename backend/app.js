ee5e04c0e235c5d8e361c962d777567295bd2eab
vkb0205 <vkb0205@gmail.com>
2026-01-12 01:53:53 +0700

    update vercel link

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     'http://localhost:5173',               // Cho phép chạy Local
-    'https://cookieproject-git-main-vkb0205s-projects.vercel.app'    // <--- THÊM LINK VERCEL CỦA BẠN VÀO ĐÂY
+    'https://cookieproject-3r3qga4mq-vkb0205s-projects.vercel.app'    // <--- THÊM LINK VERCEL CỦA BẠN VÀO ĐÂY
   ],
   credentials: true 
 }));
